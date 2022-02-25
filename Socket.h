b07e244a35f5500f331247335ba385f6bd04b7bc
Arhan Jain <arhan.j04@gmail.com>
2022-02-25 15:27:18 -0800

    added py server and minimal socket comments

--- Socket.h
+++ Socket.h
@@ -7,21 +7,21 @@ static class Socket
 {
 
          
+    public:
 
-public:
+    static  void send(std::string data)
+    {
+        // Specify socket type
+        zmq::context_t context { 1 };
+        zmq::socket_t socket { context, zmq::socket_type::req };
 
- static  void send(std::string data)
-   {
-    zmq::context_t context { 1 };
-    zmq::socket_t socket { context, zmq::socket_type::req };
-
-    socket.connect("tcp://localhost:5555");
-      std::cout << "Sending Data "
+        socket.connect("tcp://localhost:5555");
+        std::cout << "Sending Data "
                 << "..." << std::endl;
-      socket.send(zmq::buffer(data), zmq::send_flags::none);
-      zmq::message_t reply {};
-      socket.recv(reply, zmq::recv_flags::none);
-      std::cout << "Received " << reply.to_string(); 
+        socket.send(zmq::buffer(data), zmq::send_flags::none);
+        zmq::message_t reply {};
+        socket.recv(reply, zmq::recv_flags::none);
+        std::cout << "Received " << reply.to_string(); 
 
    }
 };
