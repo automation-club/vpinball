f3cdfcb0fef435d73d897a03b8069872473508f1
Allusive <auraud.zarafshar@gmail.com>
2022-02-25 15:36:35 -0800

    Fixed formatting

--- Socket.h
+++ Socket.h
@@ -3,36 +3,37 @@
 #include <iostream>
 
 #include <zmq.hpp>
- class Socket
+class Socket
 {
-    zmq::context_t context { 1 };
-    zmq::socket_t socket { context, zmq::socket_type::req };
+   zmq::context_t context { 1 };
+   zmq::socket_t socket { context, zmq::socket_type::req };
 
- public:
-      Socket() {
-         try
-         {
-            socket.connect("tcp://localhost:5555");
-         }
-         catch (zmq::error_t::exception)
-         {
-            std::cout << "Error" << std::endl;
-         }
-
-    }
-  void send(std::string data) 
+public:
+   Socket()
    {
-    try
-    {
-       std::cout << "Sending Data "
-                 << "..." << std::endl;
-       socket.send(zmq::buffer(data), zmq::send_flags::none);
-       zmq::message_t reply {};
-       socket.recv(reply, zmq::recv_flags::none);
-       std::cout << "Received " << reply.to_string(); 
-    }
-    catch (zmq::error_t::exception)
-    {
-      std::cout << "Error" << std::endl;
-    }
+      try
+      {
+         socket.connect("tcp://localhost:5555");
+      }
+      catch (zmq::error_t::exception)
+      {
+         std::cout << "Error" << std::endl;
+      }
+   }
+   void send(std::string data)
+   {
+      try
+      {
+         std::cout << "Sending Data "
+                   << "..." << std::endl;
+         socket.send(zmq::buffer(data), zmq::send_flags::none);
+         zmq::message_t reply {};
+         socket.recv(reply, zmq::recv_flags::none);
+         std::cout << "Received " << reply.to_string();
+      }
+      catch (zmq::error_t::exception)
+      {
+         std::cout << "Error" << std::endl;
+      }
+   };
 };
