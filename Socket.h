4a8132eb08b4dbeebf80df619bb34c00501c1126
Allusive <auraud.zarafshar@gmail.com>
2022-02-25 16:32:21 -0800

    Added Socket error checking

--- Socket.h
+++ Socket.h
@@ -5,35 +5,50 @@
 #include <zmq.hpp>
 class Socket
 {
+#define sockAddr "tcp://localhost:5555"
    zmq::context_t context { 1 };
    zmq::socket_t socket { context, zmq::socket_type::req };
 
 public:
    Socket()
    {
-      try
+      if (socket.handle() != nullptr)
       {
-         socket.connect("tcp://localhost:5555");
-      }
-      catch (zmq::error_t::exception)
-      {
-         std::cout << "Error" << std::endl;
+         try
+         {
+            std::cout << "Connecting to " << sockAddr << std::endl;
+            socket.connect(sockAddr);
+         }
+         catch (zmq::error_t)
+         {
+            std::cout << "Error" << &zmq::error_t::what << std::endl;
+         }
       }
    }
+     
+   
    void send(std::string data)
    {
-      try
+     
+        // std::cout << "Sending Data "<< std::endl;
+      if (&zmq::error_t::num)
       {
-         std::cout << "Sending Data "
-                   << "..." << std::endl;
          socket.send(zmq::buffer(data), zmq::send_flags::none);
          zmq::message_t reply {};
          socket.recv(reply, zmq::recv_flags::none);
-         std::cout << "Received " << reply.to_string();
       }
-      catch (zmq::error_t::exception)
+   };
+   void cleanup()
+   {
+      std::cout << "Shutting down socket" << std::endl;
+      try
       {
-         std::cout << "Error" << std::endl;
+         socket.disconnect(sockAddr);
+
+      }
+      catch (zmq::error_t)
+      {
+         std::cout << "Error disconnecting data" << &zmq::error_t::what << std::endl;
       }
    };
 };
