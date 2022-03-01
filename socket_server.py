27b820865c1148154f3c9eba182bd35188e0a822
Allusive <auraud.zarafshar@gmail.com>
2022-02-28 18:41:33 -0800

    oops I think this is the new server ver

--- socket_server.py
+++ socket_server.py
@@ -1,45 +1,46 @@
-import time
-
-from zmq.sugar.socket import Socket
-
+import random
 import zmq
 import threading
 import keyboard
 import os
 
-# Global Variables
-msg_to_send = ""
+from pathlib import Path
+from zmq.sugar.socket import Socket
 
 
 def handle_socket_server(socket: Socket):
-    global msg_to_send
+    i = 0
     while True:
-        #  Wait for next request from q client
+        #  Wait for next request from client
         message = socket.recv().decode()
-        print(f"[RECEIVED FROM CLIENT]: {message}")
+        observations = message.split(",")  # Observations: PosX, PosY, PosZ, VelX, VelY, VelZ
+        print(f"[RECEIVED FROM CLIENT]: {observations}")
 
-        #  Send reply back to client
-        socket.send(msg_to_send.encode())
-        if len(msg_to_send) > 0:
-            pass
-            # msg_to_send = ""
+        # Decide Action
+        action = None
+        if i % 30 == 0:
+            action_space = ["L", "R", "B", "N"]
+            action = random.choice(action_space)
+
+        # Send reply back to client
+        socket.send(action.encode())
 
 
-def detect_quit_keypress():
+def detect_keypress():
     while True:
         if keyboard.is_pressed('q'):
             print("Shutdown key detected.")
             break
 
 
-def launch_visual_pinball():
-    VISUAL_PINBALL_PATH = r".\x64\Debug\VPinballX.exe"
-
-    os.system(VISUAL_PINBALL_PATH)
+def launch_visual_pinball(path):
+    os.system(path)
 
 
 def main():
-    global msg_to_send
+
+    # Config
+    VISUAL_PINBALL_EXE_PATH = Path("./x64/Debug/VPinballX.exe")
 
     # Bind TCP socket and listen for clients
     context = zmq.Context()
@@ -50,29 +51,17 @@ def main():
     # Thread for handling client requests
     server_thread = threading.Thread(target=handle_socket_server, args=(socket,), daemon=True)
     # Thread for handling shutdown (press Q)
-    server_shutdown_thread = threading.Thread(target=detect_quit_keypress, args=())
+    server_shutdown_thread = threading.Thread(target=detect_keypress, args=())
     # Thread for starting Visual Pinball
-    launch_vp_thread = threading.Thread(target=launch_visual_pinball, args=())
+    launch_vp_thread = threading.Thread(target=launch_visual_pinball, args=(VISUAL_PINBALL_EXE_PATH,))
 
     # Start Threads
     server_thread.start()
     server_shutdown_thread.start()
     launch_vp_thread.start()
 
-    time.sleep(10)
-
-    msg_to_send = "L"
-
-    time.sleep(10)
-
-    msg_to_send = "N"
-    
-    time.sleep(10)
-    
-    msg_to_send = "R"
-
-    server_shutdown_thread.join()
     # Run once shutdown key detected
+    server_shutdown_thread.join()
     print("Ending program.")
 
     # Socket close handled by garbage collector
