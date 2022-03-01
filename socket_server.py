6f86248d5b3884569036370852d68a630b9d6573
Allusive <auraud.zarafshar@gmail.com>
2022-02-28 22:07:55 -0800

    Can Someone remove these commits

--- socket_server.py
+++ socket_server.py
@@ -1,32 +1,47 @@
+import random
 import zmq
 import threading
 import keyboard
 import os
 
-def handle_socket_server(socket):
+from pathlib import Path
+from zmq.sugar.socket import Socket
+
+
+def handle_socket_server(socket: Socket):
+    i = 0
     while True:
-        #  Wait for next request from qclient
-        message = socket.recv()
-        print(f"[RECEIVED FROM CLIENT]: {message}")
+        #  Wait for next request from client
+        message = socket.recv().decode()
+        observations = message.split(",")  # Observations: PosX, PosY, PosZ, VelX, VelY, VelZ
+        print(f"[RECEIVED FROM CLIENT]: {observations}")
 
-        #  Send reply back to client
-        socket.send("".encode())
+        # Decide Action
+        action = None
+        if i % 30 == 0:
+            action_space = ["L", "R", "B", "N"]
+            action = random.choice(action_space)
+
+        # Send reply back to client
+        socket.send(action.encode())
 
 
-def detect_quit_keypress():
+def detect_keypress():
     while True:
         if keyboard.is_pressed('q'):
             print("Shutdown key detected.")
             break
 
 
-def launch_visual_pinball():
-    VISUAL_PINBALL_PATH = r".\x64\Debug\VPinballX.exe"
-
-    os.system(VISUAL_PINBALL_PATH)
+def launch_visual_pinball(path):
+    os.system(path)
 
 
 def main():
+
+    # Config
+    VISUAL_PINBALL_EXE_PATH = Path("./x64/Debug/VPinballX.exe")
+
     # Bind TCP socket and listen for clients
     context = zmq.Context()
     socket = context.socket(zmq.REP)
@@ -36,17 +51,17 @@ def main():
     # Thread for handling client requests
     server_thread = threading.Thread(target=handle_socket_server, args=(socket,), daemon=True)
     # Thread for handling shutdown (press Q)
-    server_shutdown_thread = threading.Thread(target=detect_quit_keypress, args=())
+    server_shutdown_thread = threading.Thread(target=detect_keypress, args=())
     # Thread for starting Visual Pinball
-    launch_vp_thread = threading.Thread(target=launch_visual_pinball, args=())
+    launch_vp_thread = threading.Thread(target=launch_visual_pinball, args=(VISUAL_PINBALL_EXE_PATH,))
 
     # Start Threads
     server_thread.start()
     server_shutdown_thread.start()
     launch_vp_thread.start()
 
-    server_shutdown_thread.join()
     # Run once shutdown key detected
+    server_shutdown_thread.join()
     print("Ending program.")
 
     # Socket close handled by garbage collector
